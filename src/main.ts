c0d115da9e890f8e892ea19a0f248da1f25492b8
Robert Robert <doublerobert001@gmail.com>
2026-09-19 11:39:04 +0100

    replace firestore with data-connect and added server function for admin creation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-import '../styles/global.css';
+import { env } from './configs/env';
+import './styles/global.css';
+
+console.log(env.VITE_FIREBASE_APP_ID)
+console.log(env.VITE_FIREBASE_PROJECT_ID)
