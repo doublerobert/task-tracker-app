88d7c9761dbae9e8247b6b08683bcf0ce19eac74
Robert Robert <doublerobert001@gmail.com>
2026-09-20 13:55:36 +0100

    add organizations repository

---
+++ server/src/repositories/organizations.repository.ts
@@ -0,0 +1,21 @@
+import { database } from "../configs/drizzle";
+import { organizations } from "../database/schema";
+import { eq } from "drizzle-orm";
+
+export const organizationsRepository = {
+  async create(name: string) {
+    const [org] = await database
+      .insert(organizations)
+      .values({ name })
+      .returning();
+    return org;
+  },
+
+  async findById(id: string) {
+    const [org] = await database
+      .select()
+      .from(organizations)
+      .where(eq(organizations.id, id));
+    return org;
+  },
+};
